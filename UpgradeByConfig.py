c74a5c7003596decabefa98943fe3c27b5241c03
CalvinKane <kane@arista.com>
2022-06-07 15:17:11 -0400

    Changed Formatting

--- UpgradeByConfig.py
+++ UpgradeByConfig.py
@@ -1,15 +1,16 @@
+import requests.packages.urllib3
 from cvprac.cvp_client import CvpClient
 from datetime import datetime
 from uuid import uuid4
 import re, ssl
 ssl._create_default_https_context = ssl._create_unverified_context
-import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
 
 # Set Up Connection to CVP (CVaaS in this example)
 cvp_client = CvpClient()
 cvaas_token = 'Settings > Service Account > New Or Existing Account > Token'
-cvp_client.connect(nodes=['CVP IP/URL'], username='', password='', is_cvaas=True, cvaas_token=cvaas_token)
+cvp_client.connect(nodes=['CVP IP/URL'], username='',
+                   password='', is_cvaas=True, cvaas_token=cvaas_token)
 
 # Set Up CC Name and UUID
 cc_id = str(uuid4())
@@ -20,7 +21,6 @@ tasks = cvp_client.api.get_tasks_by_status('Pending')
 upgrade_tasks = []
 
 for task in tasks:
-    #print(task['data']['WORKFLOW_ACTION'])
     if task['data']['WORKFLOW_ACTION'] != "Image Push":
         continue
 
@@ -29,7 +29,7 @@ for task in tasks:
 
     configs = cvp_client.api.get_configlets_by_device_id(mac)
     for config in configs:
-        if re.search( "MLAG", config['name']):
+        if re.search("MLAG", config['name']):
             upgrade_tasks.append(task_number)
 
 print(task_number)
